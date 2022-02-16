fcb15aecf06188ccb688791dd6fecc4cee57f93e
eclecticcoding <chuck@eclecticsaddlebag.com>
2022-02-16 12:53:35 -0500

    Managing dynamic page titles

---
+++ src/_data/meta.js
@@ -0,0 +1,11 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  siteName: "EclecticCoding",
+  siteDescription: "",
+  lang: 'en',
+  locale: 'en_us',
+  authorName: 'Shane Robinson',
+  authorEmail: 'me@eclecticcoding.com',
+  twitterSite: '@eclecticcoding',
+  twitterCreator: '@eclecticcoding',
+};
