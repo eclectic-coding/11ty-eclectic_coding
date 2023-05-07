8ce120c9d1aecf95d0fe62f88fd204d180088a9c
Chuck <chuck@eclecticsaddlebag.com>
2023-05-07 14:33:36 -0400

    Add tag cloud to tag feed

--- src/_includes/components/TagButton.js
+++ src/_includes/components/TagButton.js
@@ -1,8 +1,9 @@
 const {html} = require('common-tags');
 
-function TagButton({link, linkText, ariaText}) {
+function TagButton({link, linkText, ariaText, tagCloud}) {
     return html`
-        <button class="article-card__tags" type="button" aria-label="${ariaText}">
+        <button class="${`${tagCloud ? `tagcloud` :`article-card__tags`}`}" 
+                type="button" aria-label="${ariaText}">
             <a href="${link}">${linkText}</a>
         </button>
     `;
